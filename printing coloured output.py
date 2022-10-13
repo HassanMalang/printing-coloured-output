e90a8806c799a5d13a374a96f095dac9b5a0b333
Mohamad Hassan <68748240+HassanMalang@users.noreply.github.com>
2022-10-13 22:50:53 +0500

    Add files via upload
    If you have error of no modulr colorama found then "pip install colorama" then run again code

---
+++ printing coloured output.py
@@ -0,0 +1,7 @@
+ 
+from colorama import Fore
+print (Fore.BLUE  + "HI GUYS!")
+print (Fore.GREEN +"I'm Mohammad Hassan")
+print (Fore.BLACK +"I'm a Developer")
+print (Fore.CYAN + "See you in Another code")
+
